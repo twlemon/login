469c0c65cf1627b4e357a0f99a0b2c0fca9f9816
Lemon <mengting@mail.apex.com.tw>
2022-07-02 16:37:53 +0800

    [feat] 完成登入畫面

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import background from "./static/background.jpg";
+import "./App.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUser,
+  faUnlock,
+  faCheckSquare,
+} from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={background} />
+      <main className="main">
+        <form className="login-form">
+          <h2>sign up</h2>
+          <p>Log in to get more information.</p>
+          <div className="input-group">
+            <label htmlFor="account">
+              <FontAwesomeIcon icon={faUser} />
+            </label>
+            <input
+              type="text"
+              name="account"
+              placeholder="account"
+              autoComplete="off"
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">
+              <FontAwesomeIcon icon={faUnlock} />
+            </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="password"
+              autoComplete="off"
+            />
+          </div>
+          <label className="remember-group">
+            <input
+              className="checkbox"
+              type="checkbox"
+              id="needToRememberAccount"
+            />
+            <span className="form-check-span" htmlFor="needToRememberAccount">
+              <FontAwesomeIcon icon={faCheckSquare} />
+              remember me
+            </span>
+          </label>
+          <button>log in</button>
+        </form>
+      </main>
     </div>
   );
 }
